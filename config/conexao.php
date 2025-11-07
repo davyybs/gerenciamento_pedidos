fb5a7f2b0239f68dd9a0c32e46229bb9198da47a
Davi Ferreira <daviferreira828@gmail.com>
2025-11-07 14:19:49 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -2,9 +2,10 @@
 
 $server = 'localhost';
 $user = 'root';
-$password = 'Mddr1605.';
+$password = '';
 $db = 'avaliacao_formadora_3';
 
 $conexao = mysqli_connect($server, $user, $password, $db);
+
 
 ?>
