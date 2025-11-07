ca56fcecf062cd4eea31d83ab60ead6b378e663f
Davi Ferreira <daviferreira828@gmail.com>
2025-11-07 14:22:41 -0300

    Update conexao.php
    Added error handling for database connection.

--- config/conexao.php
+++ config/conexao.php
@@ -1,11 +1,13 @@
 <?php 
-
-$server = 'localhost';
-$user = 'root';
-$password = '';
-$db = 'avaliacao_formadora_3';
-
-$conexao = mysqli_connect($server, $user, $password, $db);
-
-
+  $server = 'localhost';
+  $user = 'root';
+  $password = '';
+  $db = 'avaliacao_formadora_3';
+  
+  $conexao = mysqli_connect($server, $user, $password, $db);
+  
+  if (!$conexao) {
+      die("Falha na conexão: " . mysqli_connect_error());
+  }
 ?>
+
